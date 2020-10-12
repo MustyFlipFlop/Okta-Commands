c546bd56ec98c7a35b51c3e607a1cae55defa5c9
MustyFlipFlop <46272864+MustyFlipFlop@users.noreply.github.com>
2020-10-11 17:03:58 -0700

    Add files via upload

---
+++ OktaCommandsWk2.ps1
@@ -0,0 +1,24 @@
+﻿Connect-Okta "00nqUTefiidY53JNDWLrnzqJJCzISgdrmqjepysCSc" https://dev-ryanhightower.okta.com/
+
+$user = Get-OktaUser "me"
+
+$profile = @{login = "rm.hightower+1@gmail.com"; email = "rm.hightower+1@gmail.com"; firstName = "Ryan"; lastName = "Hightower"}
+$user = New-OktaUser @{profile = $profile}
+
+$profile = @{name = "TestGroup"; description = "This is a test"}
+$group = New-OktaGroup @{profile = $profile}
+
+$user = Get-OktaUser "me"
+$group = Get-OktaGroups "TestGroup" 'type eq "OKTA_GROUP"'
+Add-OktaGroupMember $group.id $user.id
+
+$params = @{filter = 'status eq "ACTIVE"'}
+do {
+    $page = Get-OktaUsers @params
+    $users = $page.objects
+    foreach ($user in $users) {
+        # Add more properties here:
+        Write-Host $user.profile.login $user.profile.email
+    }
+    $params = @{url = $page.nextUrl}
+} while ($page.nextUrl)
