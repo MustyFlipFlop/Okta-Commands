5102c7f8c1c438c71f6744a5cde15c34eddb09c0
MustyFlipFlop <46272864+MustyFlipFlop@users.noreply.github.com>
2020-11-07 12:51:17 -0800

    Add files via upload

---
+++ BulkUsersForLoop.ps1
@@ -0,0 +1,14 @@
+﻿Connect-Okta "TOKEN" https://dev-________.okta.com
+Get-OktaUser "me"
+function New-Users($numUsers) {
+    $now = Get-Date -Format "yyyyMMddHHmmss"
+    for ($i = 1; $i -le $numUsers; $i++) {
+        $profile = @{login="a$now$i@okta.com"; email="testuser$i@okta.com"; firstName="test"; lastName="ZExp$i"}
+        try {
+            $user = New-OktaUser @{profile = $profile; credentials = @{password = @{value = "Password123"}}} $false
+            Write-Host $i
+        } catch {
+            Get-Error $_
+        }
+    }
+}
